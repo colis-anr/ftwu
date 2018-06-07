8f2f6c00f2db6b6bef891439627d23d15cc04b39
Nicolas Jeannerod <nicolas.jeannerod@irif.fr>
2018-06-07 14:19:46 +0200

    abstract type for nfclauses + interface

--- src/back/nfclause.ml
+++ src/back/nfclause.ml
@@ -53,3 +53,46 @@ let empty_info =
     nfens = [] ;
     sims = [] ;
     nsims = [] }
+
+type t =
+  { (* [total_size] is the number of available variables. It is always
+       a power of 2. When no variable is available and a new variable
+       is required, we copy the whole structure to a structure twice
+       bigger. *)
+    total_size : int ;
+
+    (* [size] is the number of used variables. It is always smaller
+       than [total_size]. Since variables start at [0], this is also
+       the value of the first available variable. *)
+    size : int ;
+
+    (* [globals] is a map from external variables to internal classes
+       of variables. The variables in the map are in fact the free
+       variables of the constraint. Because the others internal
+       classes of variables are not accessible from the map, there is
+       no way from outside to manipulate them. *)
+    globals : Vpufs.Class.t Variable.Map.t ;
+
+    (* [infos] is a special structure provided by the [Vpufs] module
+       that associates efficiently classes of variables to their
+       information. All the infos are in the [info] type except for
+       the equalities and disequalities that are encoded directly in
+       the [Vpufs] structure. *)
+    infos : info Vpufs.t }
+
+let empty =
+  let total_size = 8 in
+  { total_size ;
+    size = 0 ;
+    globals = Variable.Map.empty ;
+    infos = Vpufs.make total_size empty_info }
+
+let double_size c =
+  (* return a copy of this structure of twice the total_size *)
+  let total_size = c.total_size lsl 1 in
+  { total_size ;
+    size = c.size ;
+    globals = c.globals ;
+    infos = Vpufs.extend c.infos total_size empty_info }
+
+exception Unsat
---
+++ src/back/nfclause.mli
@@ -0,0 +1,25 @@
+(** {1 Clauses in normal-form}
+
+   This module is here to represent clauses in normal-form.
+
+   It provides an abstract type for clauses. Any value of this type
+   that has been created using this interface is guaranteed to be in
+   normal-form.
+
+   The article "Deciding the First-Order Theory of an Algebra of
+   Feature Trees with Updates" (Nicolas Jeannerod, Ralf Treinen) then
+   tells us that such a clause is satisfiable.
+
+   Most of the functions in this interface raise the {!Unsat}
+   exception when the modification that they are supposed to make
+   would lead to an unsatisfiable clause. *)
+
+type t
+(** The abstract type of clauses in normal-form. *)
+
+val empty : t
+(** The empty clause (that is, "true"). *)
+
+exception Unsat
+(** The exception raised when a modification would lead to
+   unsatisfiability of the clause. *)
